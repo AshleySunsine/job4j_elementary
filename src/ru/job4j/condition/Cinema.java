b5c4aee219e92af1c68fb49d50f2ea88ed41af63
Ash <sgt16@yandex.ru>
2020-12-19 14:51:43 +0300

    4.3.1. Оператор && [#431065]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -11,12 +11,13 @@ public class Cinema {
     }
 
     public static void permission(boolean allowByParent, boolean hasMoney) {
-        if ((allowByParent == true) && (hasMoney == true)) {
+        if ((allowByParent) && (hasMoney)) {
             System.out.println("I can go to the cinema.");
         } else {
             System.out.println("I can't.");
         }
     }
+
     public static void main(String[] args) {
             permission(true, true);
             permission(true, false);
