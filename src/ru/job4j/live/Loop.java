d2207b574758931ec73887036035533a48ea3733
Ash <sgt16@yandex.ru>
2020-12-20 18:53:09 +0300

    5.9.1. Генерация конструкций в IDEA [#431176]

---
+++ src/ru/job4j/live/Loop.java
@@ -0,0 +1,11 @@
+package ru.job4j.live;
+
+import javax.imageio.metadata.IIOMetadataFormatImpl;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
